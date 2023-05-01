f5a807e1cb0265fbe190759a24b28087eca9309a
auhide <bo0opr@gmail.com>
2023-05-01 15:22:44 +0300

    Initial commit

---
+++ agd.py
@@ -0,0 +1,109 @@
+"""
+Automatic Gradient Descent was introduced in this paper - https://arxiv.org/pdf/2304.05187.pdf.
+I followed the implementation style of PyTorch. You can find the original implementation by the creators
+of the paper here - https://github.com/jxbz/agd.
+"""
+import math
+from typing import Iterable, Callable
+
+import torch
+from torch.nn.init import orthogonal_
+from torch.optim import Optimizer
+
+
+class AGD(Optimizer):
+
+    def __init__(self, params: Iterable[torch.Tensor], gain=1.0):
+        self.gain = gain
+        # The model will have only one optional hyperparameter, which is an acceleration
+        # parameter. It increases the update size of Automatic Gradient Descent.
+        defaults = dict(gain=gain)
+        super().__init__(params, defaults=defaults)
+        # Since the Optimizer.__init__() method by default adds the weights `params` 
+        # to an attribute `param_groups`, we take the 1st (and only) group and its
+        # parameters.
+        self.params = self.param_groups[0]["params"]
+
+    @torch.no_grad()
+    def step(self):
+        # If we are on step 1. Else, we won't initialize the weights.
+        if self.state["initialized"]:
+            self._init_weights()
+            # This parameter indicatest that we've initialized the weights.
+            self.state["initialized"] = True 
+
+        loss = agd(
+            params=self.params, 
+            get_largest_singular_value=self._scale,
+            gain=self.gain
+        )
+
+        return loss
+
+    def _init_weights(self):
+        # Looping through each weight matrix and initializing it to be uniform
+        # semi-orthogonal and re-scaled by a factor of sqrt(matrix_rows/matrix_cols).
+        # We orthogonalize the weight matrices for stability. When the gradient
+        # is backpropagated, if we use an orthogonal matrix, the gradient preserves
+        # its magnitude. 
+        for p in self.params:
+            # AGD doesn't support biases yet.
+            if p.dim() == 1:
+                raise Exception("AGD doesn't support biases.")
+            
+            self._orthogonalize(p, dim=p.dim())
+            self._scale(p)
+
+    def _orthogonalize(self, weights: torch.Tensor, dim: int):
+        # Orthogonalize a 2D weights matrix.
+        if dim == 2:
+            orthogonal_(weights)
+        # Orthogonalize a 4D weights matrix. Here we only orthogonalize each 2D
+        # tensor of the last 2 dimensions (those might be used for a Conv2d layer).
+        if dim == 4:
+            for x in range(weights.shape[2]):
+                for y in range(weights.shape[3]):
+                    orthogonal_(weights[:, :, x, y])
+
+    def _scale(self, weights: torch.Tensor):
+        # Here we are calculating the approximation of the largest singular value
+        # of `weights`. This is mentioned in Persicription 1 of the paper. It's mainly
+        # used because it simplifies the derivation of AGD.
+        singular_values_approx = math.sqrt(weights.shape[0] / weights.shape[1])
+        
+        # When `weights` is a weights matrix of a Con2d.
+        if weights.dim() == 4:
+            # This division here is done because the singular values are affected
+            # by the other two dimensions, when `weights` is 4D.
+            singular_values_approx /= math.sqrt(weights.shape[2] * weights.shape[3])
+
+        return singular_values_approx
+
+
+def agd(params: Iterable[torch.Tensor], get_largest_singular_value: Callable, gain=1.0):
+    grad_summary = 0
+    num_layers = len(list(params))
+
+    # Getting the gradient summary
+    for p in params:
+        # Here, the gradient is calculated across the first two dimensions.
+        # If we use `norm` with or without `dim=(0, 1)` for a 2D weights matrix `p`
+        # there is no difference. It only makes a difference for 4D weights tensors.
+        # In that case, we calculate the Euclidean norm accross the 1st and 2nd
+        # dimensions (`dim=(0, 1)`) which generates a 2D tensor. The values of 
+        # this tensor is then summed (`sum()`). This way we acknowledge that 
+        # the dimensionality of a 4D tensor affects the weights differently.
+        grad_summary += p.grad.norm(dim=(0, 1)).sum() * get_largest_singular_value(p)
+
+    # Normalize by the gradient using the number of layers.
+    grad_summary /= num_layers
+    learning_rate = math.log((1 + math.sqrt(1 + 4 * grad_summary)) / 2)
+
+    for p in params:
+        # Updating each weights matrix. In comparison to the loop above, we set
+        # the `keepdim` parameter to `True`. This means that when we calculate the
+        # norm for a 2D/4D `p`, the dimensionality will remain. I.e. if `p.shape` is
+        # (3, 2, 4, 2), after `p.grad.norm(dim=(0, 1))` `p.shape` will be (4, 2). But if
+        # we set `keepdim=True`, `p.shape` will be (1, 1, 4, 2). This is done so that
+        # we can divide the gradient by its norm.
+        p -= (gain * learning_rate / num_layers) * (p.grad / p.grad.norm(dim=(0, 1), keepdim=True)) * get_largest_singular_value(p)
---
+++ main.py
@@ -0,0 +1,116 @@
+"""
+Trying out my implementation of the Automatic Gradient Descent. It is implemented
+in a similar fashion to the standard PyTorch optimizers.
+"""
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+from agd import AGD
+
+
+class TestNet(nn.Module):
+    """Simple MLP network used for the testing of AGD.
+
+    Args:
+        in_size (int): The input layer size.
+        out_size (int): The output layer size.
+    """
+
+    def __init__(self, in_size, out_size):
+        super().__init__()
+
+        self.l1 = nn.Linear(in_size, 4 * in_size, bias=False)
+        self.l2 = nn.Linear(4 * in_size, out_size, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.l2(self.l1(x))
+
+        return out
+
+
+class TestDataset(Dataset):
+
+    def __init__(self, X: torch.Tensor, y: torch.LongTensor):
+        self.X, self.y = X, y
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+    def __len__(self):
+        return len(self.X)
+
+
+def train(
+    model: nn.Module, 
+    dataloader: DataLoader, 
+    loss_func: nn.Module, 
+    optimizer: torch.optim.Optimizer, 
+    epochs: int = 1, 
+    device: str = "cpu"
+):
+
+    for epoch in range(epochs):
+        progressbar = tqdm(dataloader)
+
+        for X, y in progressbar:
+            optimizer.zero_grad()
+            
+            X, y = X.to(device), y.to(device)
+            # X.shape: (BATCH_SIZE, N_FEATURES)
+            # y.shape: (BATCH_SIZE)
+            y_pred = model(X)
+            # y_pred.shape: (BATCH_SIZE, N_CLASSES)
+
+            loss = loss_func(y_pred, y)
+            loss.backward()
+
+            optimizer.step()
+
+            progressbar.set_description(f"Epoch: {epoch + 1}/{epochs}, Loss: {loss.item()}")
+
+
+# Creating a random classification dataset.
+N_FEATURES, N_CLASSES = 20, 2
+X, y = make_classification(
+    n_samples=1000, 
+    n_features=N_FEATURES, 
+    n_classes=N_CLASSES
+)
+X, y = torch.Tensor(X), torch.LongTensor(y)
+# Splitting the dataset into training and validation sets.
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8)
+
+train_dataset = TestDataset(X_train, y_train)
+train_dataloader = DataLoader(
+    dataset=train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+valid_dataset = TestDataset(X_valid, y_valid)
+valid_dataloader = DataLoader(
+    dataset=valid_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = TestNet(in_size=N_FEATURES, out_size=N_CLASSES).to(DEVICE)
+loss = nn.CrossEntropyLoss()
+# Initializing the Automatic Gradient Descent optimizer.
+optimizer = AGD(model.parameters())
+
+# Starting a training session.
+train(
+    model=model,
+    dataloader=train_dataloader,
+    loss_func=loss,
+    optimizer=optimizer,
+    epochs=3,
+    device=DEVICE
+)
